981fa3fbb9b4ddec8de27658a1ba146fd061717d
江民亮 <minliang.jiang@hoyodoo.com>
2021-02-04 22:24:54 +0800

    添加列表页面

--- src/views/jkpExpiryOrder/index.vue
+++ src/views/jkpExpiryOrder/index.vue
@@ -6,16 +6,6 @@
         <!-- 搜索 -->
         <label class="el-form-item-label">兑奖状态</label>
         <el-input v-model="query.status" clearable placeholder="兑奖状态" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
-        <label class="el-form-item-label">订单编号</label>
-        <el-input v-model="query.orderNo" clearable placeholder="订单编号" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
-        <label class="el-form-item-label">兑奖用户ID</label>
-        <el-input v-model="query.expiryUserId" clearable placeholder="兑奖用户ID" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
-        <date-range-picker
-          v-model="query.createTime"
-          start-placeholder="createTimeStart"
-          end-placeholder="createTimeStart"
-          class="date-item"
-        />
         <rrOperation :crud="crud" />
       </div>
       <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
@@ -31,12 +21,15 @@
       <!--表格渲染-->
       <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
         <el-table-column type="selection" width="55" />
-        <el-table-column prop="createTime" label="创建时间" />
-        <el-table-column prop="updateTime" label="更新时间" />
-        <el-table-column prop="ticketPhotoUrl" label="即开票图片地址" />
-        <el-table-column prop="status" label="兑奖状态" />
         <el-table-column prop="orderNo" label="订单编号" />
         <el-table-column prop="expiryUserId" label="兑奖用户ID" />
+        <el-table-column prop="ticketPhotoUrl" label="即开票图片地址" />
+        <el-table-column prop="status" label="兑奖状态">
+          <template slot-scope="scope">
+            {{ dict.label.expiry_status[scope.row.status] }}
+          </template>
+        </el-table-column>
+        <el-table-column prop="createTime" label=" 创建时间" />
         <el-table-column v-if="checkPer(['admin','jkpExpiryOrder:edit','jkpExpiryOrder:del'])" label="操作" width="150px" align="center">
           <template slot-scope="scope">
             <udOperation
@@ -60,11 +53,12 @@ import crudOperation from '@crud/CRUD.operation'
 import udOperation from '@crud/UD.operation'
 import pagination from '@crud/Pagination'
 
-const defaultForm = { id: null, createTime: null, updateTime: null, ticketPhotoUrl: null, status: null, orderNo: null, expiryUserId: null }
+const defaultForm = { id: null, orderNo: null, expiryUserId: null, ticketPhotoUrl: null, status: null, createTime: null, updateTime: null }
 export default {
   name: 'JkpExpiryOrder',
   components: { pagination, crudOperation, rrOperation, udOperation },
   mixins: [presenter(), header(), form(defaultForm), crud()],
+  dicts: ['expiry_status'],
   cruds() {
     return CRUD({ title: '即开票订单管理', url: 'api/jkpExpiryOrder', idField: 'id', sort: 'id,desc', crudMethod: { ...crudJkpExpiryOrder }})
   },
@@ -76,8 +70,14 @@ export default {
         del: ['admin', 'jkpExpiryOrder:del']
       },
       rules: {
-        createTime: [
-          { required: true, message: '创建时间不能为空', trigger: 'blur' }
+        id: [
+          { required: true, message: '订单ID不能为空', trigger: 'blur' }
+        ],
+        orderNo: [
+          { required: true, message: '订单编号不能为空', trigger: 'blur' }
+        ],
+        expiryUserId: [
+          { required: true, message: '兑奖用户ID不能为空', trigger: 'blur' }
         ],
         ticketPhotoUrl: [
           { required: true, message: '即开票图片地址不能为空', trigger: 'blur' }
@@ -85,17 +85,12 @@ export default {
         status: [
           { required: true, message: '兑奖状态不能为空', trigger: 'blur' }
         ],
-        orderNo: [
-          { required: true, message: '订单编号不能为空', trigger: 'blur' }
-        ],
-        expiryUserId: [
-          { required: true, message: '兑奖用户ID不能为空', trigger: 'blur' }
+        createTime: [
+          { required: true, message: ' 创建时间不能为空', trigger: 'blur' }
         ]
       },
       queryTypeOptions: [
-        { key: 'status', display_name: '兑奖状态' },
-        { key: 'orderNo', display_name: '订单编号' },
-        { key: 'expiryUserId', display_name: '兑奖用户ID' }
+        { key: 'status', display_name: '兑奖状态' }
       ]
     }
   },
